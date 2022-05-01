ebad60dacaaa47706bbe327105431f3723c3109b
VadikCherepanov <proger248@yandex.ru>
2022-05-01 13:40:52 +0300

    Initialized commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function show_rasp_btn1_clicked() {
+    document.location.href = "rasp2.05.html"
+}
+
+function show_rasp_btn2_clicked() {
+    document.location.href = "rasp7.05.html"
+}
+
+function show_rasp_btn3_clicked() {
+    document.location.href = "rasp8.05.html"
+}
+
+function show_raspObr_btn1_clicked() {
+    document.location.href = "rasp7.05Obr.html"
+}
+
+function show_raspObr_btn2_clicked() {
+    document.location.href = "rasp8.05Obr.html"
+}
+
+function show_raspObr_btn3_clicked() {
+    document.location.href = "rasp2.05Obr.html"
+}
+
+function home_page() {
+    document.location.href = "index.html"
+}
